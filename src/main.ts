50745a4aac28d05e6d9f0f7ee150e65225ae851d
Janderson Pizzially Vilas Boas <jandersonvb.dev@gmail.com>
2026-07-10 14:52:49 -0300

    feat: bootstrap API

---
+++ src/main.ts
@@ -0,0 +1,22 @@
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const config = app.get(ConfigService);
+
+  app.setGlobalPrefix('api');
+  app.enableCors({
+    origin: config.get<string>('FRONTEND_URL', 'http://localhost:3000'),
+    credentials: true,
+  });
+  app.useGlobalPipes(
+    new ValidationPipe({ whitelist: true, transform: true, forbidNonWhitelisted: true }),
+  );
+
+  await app.listen(config.get<number>('PORT', 3333));
+}
+
+void bootstrap();
