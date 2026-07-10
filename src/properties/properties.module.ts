c31dbdac28d25008d243d087ce4b6c9915fa1cae
Janderson Pizzially Vilas Boas <jandersonvb.dev@gmail.com>
2026-07-10 16:25:26 -0300

    wire cloudinary service

--- src/properties/properties.module.ts
+++ src/properties/properties.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
+import { CloudinaryService } from '../cloudinary/cloudinary.service';
 import { PropertiesController } from './properties.controller';
 import { PropertiesService } from './properties.service';
 
 @Module({
   imports: [AuthModule],
   controllers: [PropertiesController],
-  providers: [PropertiesService],
+  providers: [PropertiesService, CloudinaryService],
 })
 export class PropertiesModule {}
