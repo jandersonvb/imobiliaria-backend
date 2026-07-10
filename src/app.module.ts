94088f7850f788845e893a909564250bc569b139
Janderson Pizzially Vilas Boas <jandersonvb.dev@gmail.com>
2026-07-10 14:52:55 -0300

    feat: add application module

---
+++ src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { HealthController } from './health/health.controller';
+import { PrismaModule } from './prisma/prisma.module';
+import { PropertiesModule } from './properties/properties.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PrismaModule,
+    PropertiesModule,
+  ],
+  controllers: [HealthController],
+})
+export class AppModule {}
